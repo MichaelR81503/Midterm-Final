280fdac9e26d76881b82600bbe6d297113ca62a0
MichaelR81503 <144148271+MichaelR81503@users.noreply.github.com>
2023-10-22 23:12:09 -0400

    g

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float Move;
+
+    public float speed;
+    public float jump;
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+    
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+        }
+    }
+
+
+
+    public bool isGrounded()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
+    }
+}
